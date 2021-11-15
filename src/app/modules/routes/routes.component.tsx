13ca9a70b96a68cf0914b77d11c371b38f4c05ba
Andrian <andrian.kozhomin@zade.agency>
2021-11-15 17:44:51 +0200

    deploy

--- src/app/modules/routes/routes.component.tsx
+++ src/app/modules/routes/routes.component.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { RoutesProps } from './routes.props';
-import * as styles from './routes.scss';
 
 const Home = React.lazy(() => import(/* webpackChunkName: "Home" */ '@modules/home'));
 /**
  * Renders Routes
  */
 const Routes: React.FC<RoutesProps> = () => (
-  <div className={styles.routes}>
+  <div>
     <React.Suspense fallback={<div>Loading...</div>}>
       <Switch>
         <Route path='/' component={Home} />
